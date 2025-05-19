c16dddccebcc45f5b87193bb4283c22a600991ee
razzat008 <93041325+razzat008@users.noreply.github.com>
2025-05-19 16:38:54 +0545

    added: printing the tokens along with their types

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"fmt"
+
 	repl "github.com/razzat008/letsgodb/internal/REPl"
 	tok "github.com/razzat008/letsgodb/internal/Tokenizer"
 )
 
-func main(){
-// stored the initialized buffer value in line buffer
-	lineBuffer := repl.InitLineBuffer() 	
+func main() {
+	// stored the initialized buffer value in line buffer
+	lineBuffer := repl.InitLineBuffer()
 	for {
 		repl.PrintDB()
 		lineBuffer.UserInput() // repl.InitLineBuffer.UserInput
-		tok.Tokenizer(lineBuffer)
+		toks := tok.Tokenizer(lineBuffer)
+		for _, tok := range toks {
+			fmt.Printf("[%s] %s\n", tok.CurrentToken, tok.Type)
+		}
+
 		lineBuffer.Reset()
 	}
 }
