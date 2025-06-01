d0dc115b7c51bcd13f4a6a5cf5b3249f981c965f
ManogyaDahal <bat16692@gmail.com>
2025-06-01 23:35:59 +0545

    clean: Removed unnecessary decleration

--- internal/Tokenizer/tokenizer.go
+++ internal/Tokenizer/tokenizer.go
@@ -32,6 +32,7 @@ const (
 	TokenSemiColon  TokenType = "SEMICOLON"
 	TokenEOF        TokenType = "EOF" 
 	TokenComma 			TokenType = "COMMA"
+	TokenAsterisk 	TokenType = "ASTERISK"
 )
 
 // tokenizes the user input []byte into string
@@ -55,6 +56,8 @@ func Tokenizer(lb *repl.LineBuffer) []Token {
 			tokens = append(tokens, Token{Type: TokenSemiColon, CurrentToken: upperToken})
 		case ",": 
 			tokens = append(tokens, Token{Type: TokenComma, CurrentToken: upperToken})
+		case "*": 
+			tokens = append(tokens, Token{Type: TokenAsterisk, CurrentToken: upperToken})
 		default:
 			if strings.HasPrefix(currentToken, "'") && strings.HasSuffix(currentToken, "'") {
 				// checking for values like 'School' ; i.e. quoted values
